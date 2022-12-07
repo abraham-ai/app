dfd69488a32f98014b8a7dc379c086ae61de44bf
Gene Kogan <kogan.gene@gmail.com>
2022-12-06 16:29:28 -0800

    switch back to gateway

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 import React, {useState, useCallback} from 'react';
 import {Tabs} from 'antd';
 
-const GATEWAY_URL ="https://app.dev.aws.abraham.fun";// "https://gateway-test.abraham.ai"; //"https://app.dev.aws.abraham.fun"
+const GATEWAY_URL = "https://gateway-test.abraham.ai"; //"https://app.dev.aws.abraham.fun"
 const MINIO_URL = "https://minio.aws.abraham.fun";
 const MINIO_BUCKET = "creations-stg";
 
