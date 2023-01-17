e30b81a9a7987fee132d2c1e741ffbc01b7958cd
Gene Kogan <kogan.gene@gmail.com>
2023-01-16 22:16:17 -0800

    move balance to account tab

--- src/components/tabs/AccountTab/AccountTab.tsx
+++ src/components/tabs/AccountTab/AccountTab.tsx
@@ -2,7 +2,7 @@ import { Divider } from "antd";
 import React from "react";
 
 import SignIn from "components/tabs/AccountTab/SignIn";
-import CreditBalance from "components/CreditBalance";
+import CreditBalance from "components/tabs/AccountTab/CreditBalance";
 import ApiKeys from "components/tabs/AccountTab/ApiKeys";
 
 const AccountTab = () => {
