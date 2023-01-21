fde35fb0324f3bfe7845b44efe6977c5fdd5f99b
masoncm97 <masoncmathew@gmail.com>
2023-01-20 18:48:36 -0800

    Added media upload

--- src/pages/api/media.ts
+++ src/pages/api/media.ts
@@ -1,69 +1,55 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { withSessionRoute } from "util/withSession";
 import formidable from "formidable";
+import { uploadMedia } from "util/eden";
+import fs from 'fs';
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-
-  console.log("handle 11")
-  console.log(req.method)
-  if (req.method === 'POST') {
-    console.log("handle 12")
-    const form = new formidable.IncomingForm();
-    console.log("handle 12a")
-    form.parse(req, (err, fields, files) => {
-      console.log("go!")
-
-      if (err) {
-        console.log("err")
-        res.status(500).json({error: "Unable to upload file"})
-        return;
-      }
-      // File saved to `files.file.path`
-      const file = files.file;
-      const filePath = `public/uploads/${file.name}`;
-      
-      console.log("=======")
-      console.log(file);
-      console.log(filePath);
-
-      res.status(200).json({ fileUrl: `/uploads/${file.name}` })
-
-      // Move file to public/uploads directory
-      /*fs.rename(file.path, filePath, (err) => {
-        if (err) {
-          res.status(500).json({error: "Unable to move file"})
-        }
-        res.status(200).json({ fileUrl: `/uploads/${file.name}` });
-      });*/
-      
-    });
-  } else {
-    console.log("handle 13")
-    res.status(405).json({error: "Method not allowed"})
-  }
-
-  console.log("handle 14")
-
-  // console.log(req.body.file)
-  // const { reqConfig } = req.body;
-  // const authToken = req.session.token;
-
-  // const config = {
-  //   generatorName: "create",
-  //   requestConfig: reqConfig,
-  // };
+  const authToken = req.session.token;
 
   // if (!authToken) {
   //   return res.status(401).json({ error: "Not authenticated" });
   // }
 
-  // try {
-  //   const result = await getGatewayResult(config, authToken);
-  //   return res.status(200).json({ outputUrl: result.outputUrl });
-  // } catch (error: any) {
-  //   console.error(error);
-  //   return res.status(500).json({ error: error.response.data });
-  // }
-};
+  if (req.method === 'POST') {
+
+    const form = new formidable.IncomingForm();
+
+    form.parse(req, async (err, fields, files) => {
+
+      if (err) {
+        return res.status(500).json({
+          error: `Error parsing form`,
+        });
+      }
+
+      const file = files.file as formidable.File;
+      const newFilePath = `public/uploads/${file.originalFilename}`;
+
+      fs.rename(file.filepath, newFilePath, (err) => {
+        if (err) {
+          res.status(500).json({error: "Unable to move file"})
+        }
+      });
+
+      try {
+        const result = await uploadMedia(authToken!, newFilePath, file.originalFilename);
+        res.status(200).json({ fileUrl: `/uploads/${file.originalFilename}` })
+      } catch (error: any) {
+        return res.status(500).json({ error: error.response?.data });
+      }
+    });
+
+  } else {
+    res.status(405).json({ error: "Method not allowed" })
+  }
+
+}
 
 export default withSessionRoute(handler);
