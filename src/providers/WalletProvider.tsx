501ae2fe6dbc7635b03c79b85570e30aa0ecfa46
Gene Kogan <kogan.gene@gmail.com>
2023-02-14 20:58:36 -0800

    mainnet

--- src/providers/WalletProvider.tsx
+++ src/providers/WalletProvider.tsx
@@ -3,12 +3,12 @@ import "@rainbow-me/rainbowkit/styles.css";
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { PropsWithChildren } from "react";
 import { configureChains, createClient, WagmiConfig } from "wagmi";
-import { goerli } from "wagmi/chains";
+import { mainnet } from "wagmi/chains";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 
 const { chains, provider } = configureChains(
-  [goerli],
+  [mainnet],
   [
     alchemyProvider({
       apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY as string,
