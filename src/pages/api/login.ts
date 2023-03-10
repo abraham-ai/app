40525043440ddd40a8f7b456f3e01d970a5b0a9b
Gene Kogan <kogan.gene@gmail.com>
2023-03-09 16:26:48 -0800

    fix undefined authtoken

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -14,6 +14,10 @@ const handler = async (req: ApiRequest, res: NextApiResponse) => {
   const { message, signature, userAddress } = req.body;
   const authToken = req.session.token;
 
+  if (!authToken) {
+    return res.status(401).json({ error: "Not authenticated" });
+  }
+
   try {
     const eden = new EdenClient();
     eden.setAuthToken(authToken);
