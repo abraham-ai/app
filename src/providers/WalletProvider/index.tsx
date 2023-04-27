0c876a87caa7b93e78e324171d4011650c57a53b
Jonathan Miller <jon@jmill.dev>
2023-04-26 23:03:20 -0400

    Fix provider

--- src/providers/WalletProvider/index.tsx
+++ src/providers/WalletProvider/index.tsx
@@ -5,17 +5,15 @@ import { PropsWithChildren } from "react";
 const BLOCKCHAIN_ENV = process.env.NEXT_PUBLIC_BLOCKCHAIN_ENV;
 
 const WalletProvider = ({ children }: PropsWithChildren) => {
-  switch (BLOCKCHAIN_ENV) {
-    case "goerli":
-      console.log("goerli");
-      return <WalletProviderGoerli>{children}</WalletProviderGoerli>;
-    case "mainnet":
-      console.log("mainnet");
-      return <WalletProviderMainnet>{children}</WalletProviderMainnet>;
-    default:
-      console.log("default");
-      return <WalletProviderMainnet>{children}</WalletProviderMainnet>;
-  }
+  return (
+    <>
+      {BLOCKCHAIN_ENV === "goerli" ? (
+        <WalletProviderGoerli>{children}</WalletProviderGoerli>
+      ) : (
+        <WalletProviderMainnet>{children}</WalletProviderMainnet>
+      )}
+    </>
+  );
 };
 
 export default WalletProvider;
